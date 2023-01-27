234ad372e86145c0ff2227de86e8c5c7a966a202
Megan Ellman <meganlellman@gmail.com>
2023-01-26 21:03:35 -0800

    fix method test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,21 +1,22 @@
 const Engineer = require('../lib/engineer');
-
+afterEach(() => {
+    jest.restoreAllMocks()
+  });
 describe('Engineer Class', () => {
+    const engineer = new Engineer('Maya', 123, 'maya@gmail.com', 'mayagithub');
+    jest.spyOn(engineer, 'getGithub')
+    jest.spyOn(engineer, 'getRole')
+    jest.spyOn(engineer, 'validateEmail')
     it('fourth parameter should be the value of github username', () => {
-        const engineer = new Engineer('Maya', 123, 'maya@email.com', 'mayagithub');
-
         expect(engineer.github).toEqual('mayagithub');
     });
-    test('should return github username', () => {
-        const engineer = new Engineer('Maya', 123, 'maya@email.com', 'mayagithub');
-        jest.spyOn(engineer, 'getGithub').mockImplementation(() => 'mayagithub');
-      
-        expect(engineer.getGithub()).toBe('mayagithub');
+    it('should return github username', () => {
+        expect(engineer.getGithub()).toEqual('mayagithub');
       });
-    test('should return Engineer', () => {
-        const engineer = new Engineer('Maya', 123, 'maya@email.com', 'mayagithub');
-        jest.spyOn(engineer, 'getRole').mockImplementation(() => 'Engineer');
-      
-        expect(engineer.getRole()).toBe('Engineer');
+    it('should return Engineer', () => {
+        expect(engineer.getRole()).toEqual('Engineer');
     })
+    it('should return true if email is valid', () => {
+        expect(engineer.validateEmail()).toBe(true);
+      });
 })
