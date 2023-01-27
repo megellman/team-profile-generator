8103f567d7883833aebe3a3a958802d87e8066a4
Megan Ellman <meganlellman@gmail.com>
2023-01-26 21:04:00 -0800

    fixed method test

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,19 +1,23 @@
 const Manager = require('../lib/manager');
+afterEach(() => {
+  jest.restoreAllMocks()
+});
 describe('Manager Class', () => {
-    it('fourth parameter should be the value of officeNumber', () => {
-        const manager = new Manager('Ann', 123, 'ann@email.com', 555);
-        expect(manager.officeNumber).toEqual(555);
-    });
-    test('should return office number', () => {
-        const manager = new Manager('Ann', 123, 'ann@email.com', 555);
-        jest.spyOn(manager, 'getOfficeNumber').mockImplementation(() => 555);
-      
-        expect(manager.getOfficeNumber()).toBe(555);
-      });
-    test('should return Manager', () => {
-        const manager = new Manager('Ann', 123, 'ann@email.com', 555);
-        jest.spyOn(manager, 'getRole').mockImplementation(() => 'Manager');
-      
-        expect(manager.getRole()).toBe('Manager');
-      });
+  const manager = new Manager('Ann', 123, 'ann@email.com', 555);
+  jest.spyOn(manager, 'getRole')
+  jest.spyOn(manager, 'getOfficeNumber')
+  jest.spyOn(manager, 'validateEmail')
+
+  it('fourth parameter should be the value of officeNumber', () => {
+    expect(manager.officeNumber).toEqual(555);
+  });
+  it('should return office number', () => {
+    expect(manager.getOfficeNumber()).toEqual(555);
+  });
+  it('should return Manager', () => {
+    expect(manager.getRole()).toEqual('Manager');
+  });
+  it('should return true if email is valid', () => {
+    expect(manager.validateEmail()).toBe(true);
+  });
 })
