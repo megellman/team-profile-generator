2167fdf08cb2dd3852661b32177b5f6463a7e930
Megan Ellman <meganlellman@gmail.com>
2023-01-26 21:03:47 -0800

    fixed method test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,21 +1,22 @@
 const Intern = require('../lib/intern');
-
+afterEach(() => {
+    jest.restoreAllMocks()
+  });
 describe('Intern Class', () => {
+    const intern = new Intern('Lo', 444, 'email@email.com', 'schoolOfSchooling');
+    jest.spyOn(intern, 'getSchool')
+    jest.spyOn(intern, 'getRole')
+    jest.spyOn(intern, 'validateEmail')
     it('Should return school', () => {
-        const intern = new Intern('Lo', 444, 'email@email.com', 'schoolOfSchooling');
-
         expect(intern.school).toEqual('schoolOfSchooling');
     });
-    test('should return school', () => {
-        const intern = new Intern('Lo', 444, 'email@email.com', 'schoolOfSchooling');
-        jest.spyOn(intern, 'getSchool').mockImplementation(() => 'schoolOfSchooling');
-      
-        expect(intern.getSchool()).toBe('schoolOfSchooling');
-      });
-    test('should return Intern', () => {
-        const intern = new Intern('Lo', 444, 'email@email.com', 'schoolOfSchooling');
-        jest.spyOn(intern, 'getRole').mockImplementation(() => 'Intern');
-      
-        expect(intern.getRole()).toBe('Intern');
+    it('should return school', () => {
+        expect(intern.getSchool()).toEqual('schoolOfSchooling');
+    });
+    it('should return Intern', () => {
+        expect(intern.getRole()).toEqual('Intern');
     })
+    it('should return true if email is valid', () => {
+        expect(intern.validateEmail()).toBe(true);
+      });
 })
