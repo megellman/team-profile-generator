35fd8ea2bd1621c275d3e0a1ceaf7f7d0e3958a8
Megan Ellman <meganlellman@gmail.com>
2023-01-26 21:03:20 -0800

    fix tests for methods

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,40 +1,39 @@
 const Employee = require('../lib/employee');
-    
+afterEach(() => {
+  jest.restoreAllMocks()
+});
 describe('Employee Class', () => {
   it('first parameter should be the value of name', () => {
-    const name = 'Joe';
-    const employee = new Employee(name);
+    const employee = new Employee('Joe', 123, 'test@gmail.com');
     expect(employee.name).toEqual('Joe');
   });
   it('second parameter should be the value of id', () => {
-    const name = 'sample';
-    const id = 123;
-    const employee = new Employee(name, id);
+    const employee = new Employee('Joe', 123, 'test@gmail.com');
     expect(employee.id).toEqual(123);
   });
   it('third parameter should be the value of email', () => {
-    const name = 'sample';
-    const id = 111;
-    const email = 'test@email.com';
-    const employee = new Employee(name, id, email);
-    expect(employee.email).toEqual('test@email.com');  
-  });
-  test('should return name', () => {
     const employee = new Employee('Joe', 123, 'test@gmail.com');
-    jest.spyOn(employee, 'getName').mockImplementation(() => 'Joe');
+    expect(employee.email).toEqual('test@gmail.com');
+  });
+  describe('methods should return correct values', () => {
+    const employee = new Employee('Joe', 123, 'test@gmail.com');
 
-    expect(employee.getName()).toBe('Joe');
-  });
-  test('should return email', () => {
-    const employee = new Employee('Joe', 123, 'test@gmail.com');
-    jest.spyOn(employee, 'getName').mockImplementation(() => 'test@gmail.com');
+    jest.spyOn(employee, 'getName')
+    jest.spyOn(employee, 'getEmail')
+    jest.spyOn(employee, 'getRole')
+    jest.spyOn(employee, 'validateEmail')
 
-    expect(employee.getEmail()).toBe('test@gmail.com');
-  });
-  test('should return Employee', () => {
-    const employee = new Employee('Joe', 123, 'test@gmail.com');
-    jest.spyOn(employee, 'getRole').mockImplementation(() => 'Employee');
-  
-    expect(employee.getRole()).toBe('Employee');
-  });
+    it('should return the name', () => {
+      expect(employee.getName()).toEqual('Joe');
+    });
+    it('should return the email', () => {
+      expect(employee.getEmail()).toEqual('test@gmail.com');
+    });
+    it('should return the role', () => {
+      expect(employee.getRole()).toEqual('Employee');
+    });
+    it('should return true if email is valid', () => {
+      expect(employee.validateEmail()).toBe(true);
+    });
+  })
 })
